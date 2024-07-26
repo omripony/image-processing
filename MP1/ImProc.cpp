67ec535fd064cfad3598c5b716246fe264abae86
omripony <omripony7763@gmail.com>
2024-07-26 18:36:08 +0300

    MP1 update (major)

--- MP1/ImProc.cpp
+++ MP1/ImProc.cpp
@@ -1,14 +1,6 @@
-/*
-  First steps in Image Processing
-  Creating simple Synthetic Images
-  Using BMP Library
-*/
-
-
 #include <stdio.h> // for printf
 #include <conio.h> // for getch
 
-#include <iostream> // for cin cout
 #include <fstream>  // For file IO
 using namespace std; // Explain some day 
 
@@ -20,74 +12,78 @@ using namespace std; // Explain some day
 #include "ImProcInPlainC.h"
 
 
-
-
-void  lightness(unsigned char image[][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
+void lightness(unsigned char image[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
 {
-	int Red, Blue, Green;
-	for (int row = 0; row < CGA_HEIGHT; row++)
-	{
-		for (int col = 0; col < CGA_WIDTH; col++)
-		{
-			Red = image[row][col][R];
-			Green = image[row][col][G];
-			Blue = image[row][col][B];
-			image[row][col][R] = int((max(max(Red, Blue), Green) + (min(min(Red, Blue), Green))) / 2.0);
-			image[row][col][G] = int((max(max(Red, Blue), Green) + (min(min(Red, Blue), Green))) / 2.0);
-			image[row][col][B] = int((max(max(Red, Blue), Green) + (min(min(Red, Blue), Green))) / 2.0);
-		}																		  
-	}
-}
-void  average(unsigned char image[][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
-{
-	int Red, Blue, Green;
-	for (int row = 0; row < CGA_HEIGHT; row++)
-	{
-		for (int col = 0; col < CGA_WIDTH; col++)
-		{
-			Red = image[row][col][R];
-			Green = image[row][col][G];
-			Blue = image[row][col][B];
-			image[row][col][R] = int((Red + Blue + Green) / 3.0);
-			image[row][col][G] = int((Red + Blue + Green) / 3.0);
-			image[row][col][B] = int((Red + Blue + Green) / 3.0);
-		}
-	}
-}
-void luminosity(unsigned char image[][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
-{
-	int Red, Blue, Green;
+	unsigned char* pixel = &image[0][0][0];
 	for (int row = 0; row < NUMBER_OF_ROWS; row++)
 	{
 		for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
 		{
-			Red = image[row][col][R];
-			Green = image[row][col][G];
-			Blue = image[row][col][B];
-			image[row][col][R] = int((int(0.21 * Red) + int(0.72 * Green) + int(0.07 * Blue)));
-			image[row][col][G] = int((int(0.21 * Red) + int(0.72 * Green) + int(0.07 * Blue)));
-			image[row][col][B] = int((int(0.21 * Red) + int(0.72 * Green) + int(0.07 * Blue)));
+			int Red = *(pixel + R);
+			int Green = *(pixel + G);
+			int Blue = *(pixel + B);
+			int lightness = (max(max(Red, Blue), Green) + min(min(Red, Blue), Green)) / 2;
+			*(pixel + R) = *(pixel + G) = *(pixel + B) = lightness;
+			pixel += NUMBER_OF_COLORS;
 		}
 	}
 }
 
-void singleColorChannel(unsigned char image[][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
+
+void average(unsigned char image[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
 {
+	unsigned char* pixel = &image[0][0][0];
 	for (int row = 0; row < NUMBER_OF_ROWS; row++)
 	{
 		for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
 		{
-			int green = image[row][col][G];
-			image[row][col][R] = green;
-			image[row][col][G] = green;
-			image[row][col][B] = green;
+			int Red = *(pixel + R);
+			int Green = *(pixel + G);
+			int Blue = *(pixel + B);
+			int avg = (Red + Green + Blue) / 3;
+			*(pixel + R) = *(pixel + G) = *(pixel + B) = avg;
+			pixel += NUMBER_OF_COLORS;
 		}
 	}
 }
+
+
+void luminosity(unsigned char image[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
+{
+	unsigned char* pixel = &image[0][0][0];
+	for (int row = 0; row < NUMBER_OF_ROWS; row++)
+	{
+		for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
+		{
+			int Red = *(pixel + R);
+			int Green = *(pixel + G);
+			int Blue = *(pixel + B);
+			int lum = int(0.21 * Red + 0.72 * Green + 0.07 * Blue);
+			*(pixel + R) = *(pixel + G) = *(pixel + B) = lum;
+			pixel += NUMBER_OF_COLORS;
+		}
+	}
+}
+
+
+void singleColorChannel(unsigned char image[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS])
+{
+	unsigned char* pixel = &image[0][0][0];
+	for (int row = 0; row < NUMBER_OF_ROWS; row++)
+	{
+		for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
+		{
+			int green = *(pixel + G);
+			*(pixel + R) = *(pixel + G) = *(pixel + B) = green;
+			pixel += NUMBER_OF_COLORS;
+		}
+	}
+}
+
 
 void main()
 {
-	unsigned char Picture[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS];
+	static unsigned char Picture[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS][NUMBER_OF_COLORS];
 
 	//First picture
 	LoadBgrImageFromTrueColorBmpFile(Picture, "1.bmp");
@@ -123,94 +119,4 @@ void main()
 	singleColorChannel(Picture);
 	StoreBgrImageAsGrayBmpFile(Picture, "2_singleColorChannel.bmp");
 
-	//Third picture
-	LoadBgrImageFromTrueColorBmpFile(Picture,"3.bmp");
-	average(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "3_averge.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "3.bmp");
-	lightness(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "3_lightness.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "3.bmp");
-	luminosity(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "3_luminosity.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "3.bmp");
-	singleColorChannel(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "3_singleColorChannel.bmp");
-
-	//fourth picture
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "4.bmp");
-	average(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "4_averge.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "4.bmp");
-	lightness(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "4_lightness.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "4.bmp");
-	luminosity(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "4_luminosity.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "4.bmp");
-	singleColorChannel(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "4_singleColorChannel.bmp");
-
-	//fifth picture
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "5.bmp");
-	average(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "5_averge.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "5.bmp");
-	lightness(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "5_lightness.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "5.bmp");
-	luminosity(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "5_luminosity.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "5.bmp");
-	singleColorChannel(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "5_singleColorChannel.bmp");
-
-	////sixth picture
-
-	//LoadBgrImageFromTrueColorBmpFile(Picture, "6.bmp");
-	//average(Picture);
-	//StoreBgrImageAsGrayBmpFile(Picture, "6_averge.bmp");
-
-	//LoadBgrImageFromTrueColorBmpFile(Picture, "6.bmp");
-	//lightness(Picture);
-	//StoreBgrImageAsGrayBmpFile(Picture, "6_lightness.bmp");
-
-	//LoadBgrImageFromTrueColorBmpFile(Picture, "6.bmp");
-	//luminosity(Picture);
-	//StoreBgrImageAsGrayBmpFile(Picture, "6_luminosity.bmp");
-
-	//LoadBgrImageFromTrueColorBmpFile(Picture, "6.bmp");
-	//singleColorChannel(Picture);
-	//StoreBgrImageAsGrayBmpFile(Picture, "6_singleColorChannel.bmp");
-	
-	//seventh picture
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "7.bmp");
-	average(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "7_averge.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "7.bmp");
-	lightness(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "7_lightness.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "7.bmp");
-	luminosity(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "7_luminosity.bmp");
-
-	LoadBgrImageFromTrueColorBmpFile(Picture, "7.bmp");
-	singleColorChannel(Picture);
-	StoreBgrImageAsGrayBmpFile(Picture, "7_singleColorChannel.bmp");
-
-	WaitForUserPressKey();
 }
